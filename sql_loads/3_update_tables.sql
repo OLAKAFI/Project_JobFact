f499a2f85ca7471c704a4a1231921769d2281d52
OLAKAFI <olatunbosun.kafi@gmail.com>
2024-08-07 21:04:28 +0100

    file reorganized

--- sql_loads/3_update_tables.sql
+++ sql_loads/3_update_tables.sql
@@ -26,19 +26,19 @@ NOTE: If you are having issues with permissions. And you get error:
 -- NOTE: This has been updated from the video to fix issues with encoding
 
 COPY company_dim
-FROM 'C:/Users/olatu/Desktop/Project_JobFact_SQL/csv_files/company_dim.csv'
+FROM 'C:/Users/olatu/Desktop/LUKE SQL/csv_files/company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY job_postings_fact
-FROM 'C:/Users/olatu/Desktop/Project_JobFact_SQL/csv_files/job_postings_fact.csv'
+FROM 'C:/Users/olatu/Desktop/LUKE SQL/csv_files/job_postings_fact.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_dim
-FROM 'C:/Users/olatu/Desktop/Project_JobFact_SQL/csv_files/skills_dim.csv'
+FROM 'C:/Users/olatu/Desktop/LUKE SQL/csv_files/skills_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 COPY skills_job_dim
-FROM 'C:/Users/olatu/Desktop/Project_JobFact_SQL/csv_files/skills_job_dim.csv'
+FROM 'C:/Users/olatu/Desktop/LUKE SQL/csv_files/skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 select *
